62743dfd643c0623a4c77de5ca1b5e9773aa9518
shotgun45 <jipeles@yahoo.com>
2025-07-15 20:44:39 -0400

    chapter 2 lesson 6

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,5 @@
 import unittest
-
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -29,6 +28,64 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("Link text", TextType.LINK, "https://example.com")
         node2 = TextNode("Link text", TextType.LINK, "https://example.com")
         self.assertEqual(node, node2)
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("Bold!", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "Bold!")
+
+    def test_italic(self):
+        node = TextNode("Italic!", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "Italic!")
+
+    def test_code(self):
+        node = TextNode("print('hi')", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "print('hi')")
+
+    def test_link(self):
+        node = TextNode("anchor", TextType.LINK, "https://example.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "anchor")
+        self.assertEqual(html_node.props["href"], "https://example.com")
+
+    def test_link_missing_url(self):
+        node = TextNode("anchor", TextType.LINK)
+        with self.assertRaises(Exception):
+            text_node_to_html_node(node)
+
+    def test_image(self):
+        node = TextNode("alt text", TextType.IMAGE, "https://img.com/img.png")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props["src"], "https://img.com/img.png")
+        self.assertEqual(html_node.props["alt"], "alt text")
+
+    def test_image_missing_url(self):
+        node = TextNode("alt text", TextType.IMAGE)
+        with self.assertRaises(Exception):
+            text_node_to_html_node(node)
+
+    def test_invalid_type(self):
+        class FakeType:
+            pass
+        node = TextNode("text", FakeType())
+        with self.assertRaises(Exception):
+            text_node_to_html_node(node)
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,4 @@
+from htmlnode import LeafNode
 from enum import Enum
 
 class TextType(Enum):
@@ -25,3 +26,26 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+    if not isinstance(text_node, TextNode):
+        raise Exception("Input must be a TextNode")
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    elif text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    elif text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    elif text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    elif text_node.text_type == TextType.LINK:
+        if not text_node.url:
+            raise Exception("LINK TextNode must have a url")
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    elif text_node.text_type == TextType.IMAGE:
+        if not text_node.url:
+            raise Exception("IMAGE TextNode must have a url")
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    else:
+        raise Exception(f"Unknown TextType: {text_node.text_type}")
